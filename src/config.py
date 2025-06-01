9f5b43e2a50e795312dde7def9f2c3feeae68b52
Mateus Pontes <mateuspsdd@gmail.com>
2025-06-01 13:28:57 -0300

    refactor

--- src/config.py
+++ src/config.py
@@ -1,7 +1,18 @@
-import os, json
+import os, json, sys
 
 
-def load_extension_mapping(config_path='config.json') -> dict:
+def get_base_dir():
+    if getattr(sys, 'frozen', False):
+        return os.path.dirname(sys.executable) # Rodando como .exe (PyInstaller)
+
+    main_file: str = sys.argv[0]
+    main_file_abs_path: str = os.path.abspath(main_file)
+    return os.path.dirname(main_file_abs_path)
+
+def load_extension_mapping(config_filename='config.json') -> dict:
+    base_dir = get_base_dir()
+    config_path = os.path.join(base_dir, config_filename)
+
     if not os.path.exists(config_path):
         raise FileNotFoundError(f"Config file not found: {config_path}")
 
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,28 @@
+import json
+
+
+
 def get_path_str(path: str | bytes) -> str:
     if isinstance(path, bytes):
         return path.decode('utf-8', errors='replace')
     return path
+
+def get_targets(configs: dict) -> list[str]:
+    targets: list[str] = configs.get("targets")
+    if not targets:
+        raise ValueError("TARGETS environment variable is not set or empty.")
+    return targets
+
+def display_start_message(extension_to_dir_map: dict):
+    print("=" * 50)
+    print("🗂️  File Watcher Mover")
+    print("Directory configuration:")
+    print(json.dumps(extension_to_dir_map, ensure_ascii=False, indent=2))
+    print("Press Ctrl+C to exit.")
+    print("=" * 50)
+
+def get_extensions_map(configs: dict) -> dict:
+    extension_map: dict = configs.get("extension")
+    if not extension_map:
+        raise ValueError("TARGETS environment variable is not set or empty.")
+    return extension_map
