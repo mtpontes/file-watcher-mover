349a07b77c5e1de3f4edbd6c2ceb0c2c537fe439
Mateus Pontes <mateuspsdd@gmail.com>
2025-06-07 16:05:27 -0300

    refactor: reorganize config loading and enhance target retrieval logic

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from watchdog.observers import Observer
 
 from src.handler import DirHandler
-from src.config import load_extension_mapping
+from src.config import load_extension_mapping, get_targets, get_extensions_map
+from src.utils import display_start_message
 
 
-def run():
-    configs: dict = load_extension_mapping()
-    targets: list[str] = get_targets(configs)
-    extension_to_dir_map: dict = get_extensions_map(configs)
-    
-    event_handler = DirHandler(extension_to_dir_map)
+def start_observer(targets: list[str], handler: DirHandler):
     observer = Observer()
-    
+
     for target_dir in targets:
-        observer.schedule(event_handler=event_handler, path=target_dir)
-        
+        observer.schedule(event_handler=handler, path=target_dir)
+
     observer.start()
     try:
         while observer.is_alive():
@@ -22,18 +18,20 @@ def run():
     finally:
         observer.stop()
         observer.join()
-        
-def get_targets(configs: dict) -> list[str]:
-    targets: list[str] = configs.get("targets")
-    if not targets:
-        raise ValueError("TARGETS environment variable is not set or empty.")
-    return targets
 
-def get_extensions_map(configs: dict) -> dict:
-    extension_map: dict = configs.get("extension")
-    if not extension_map:
-        raise ValueError("TARGETS environment variable is not set or empty.")
-    return extension_map
+def run():
+    configs: dict = load_extension_mapping()
+    targets: list[str] = get_targets(configs)
+    extension_to_dir_map: dict = get_extensions_map(configs)
+    display_start_message(configs)
+
+    event_handler = DirHandler(extension_to_dir_map)
+    start_observer(targets, event_handler)
+    
 
 if __name__ == "__main__":
-    run()
+    try:
+        run()
+    except Exception as e:
+        print(e)
+        pass
